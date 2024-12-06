47f964377dcfd375e837bfe38539ea7174b382f1
suri312006 <suri312006@gmail.com>
2024-12-06 00:56:06 -0800

    pretty much done with designdoc

--- src/keys.rs
+++ src/keys.rs
@@ -16,7 +16,6 @@ impl VerifyingKey {
                 let signing_key = SigningKey::from_slice(target_priv_key)
                     .map_err(|_| CapError::InvalidPrivateKey)?;
                 let vkey = p256::ecdsa::VerifyingKey::from(signing_key);
-
                 let mut buff = [0; 1024];
                 let len = 33;
                 buff[0..len].copy_from_slice(vkey.to_encoded_point(true).as_bytes());
@@ -28,6 +27,7 @@ impl VerifyingKey {
             }
         }
     }
+    // so we can easily extract out the key without worrying about len and the buffer
     pub fn as_bytes(&self) -> &[u8] {
         &self.key[0..self.len as usize]
     }
